150dcba5f6f6bee18692f7fdf7ed3f5053a8b109
BStchw <93220422+BStchw@users.noreply.github.com>
2025-04-09 12:19:14 +0200

    4.0

--- Projekt2/my-play-app/app/controllers/HomeController.scala
+++ Projekt2/my-play-app/app/controllers/HomeController.scala
@@ -4,6 +4,8 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import repositories.ProductRepository
+import repositories.CategoryRepository
+import repositories.CartRepository
 import scala.concurrent.ExecutionContext
 
 /**
@@ -13,11 +15,15 @@ import scala.concurrent.ExecutionContext
 @Singleton
 class HomeController @Inject()(
                                 val controllerComponents: ControllerComponents,
-                                productRepo: ProductRepository
+                                productRepo: ProductRepository,
+                                categoryRepo: CategoryRepository,
+                                cartRepo: CartRepository
 
                               )(implicit ec: ExecutionContext) extends BaseController {
 
   productRepo.init()
+  categoryRepo.init()
+  cartRepo.init()
 
   def index = Action {
     Ok("Strona główna działa!")
